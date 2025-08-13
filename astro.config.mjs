223b57a4883c9388cc429e5562a22bb261824e5b
Daniel <dan1294x@gmail.com>
2025-08-12 21:59:12 -0400

    images folder moved from public to src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dan1294x.github.io/NonDesignerBook/', // URL final
+    base: '/NonDesignerBook/', 
+});
